c864e3c98d8e6514c29dcd35408aa64ee9e10a70
zhendozzz <info@zhendozzz.ru>
2020-10-26 19:35:28 +0300

    add scroll to end detection

--- src/pages/HostPoints/index.js
+++ src/pages/HostPoints/index.js
@@ -2,18 +2,25 @@ import React from 'react'
 import s from './HostPoints.module.scss'
 import HostPoint from '../../components/HostPoint'
 
-const Organisations = ({ data = [], history }) => {
+const HostPoints = ({ data = [], history }) => {
+  const handleScroll = (e) => {
+    const { target } = e
+    const { scrollHeight, scrollTop, clientHeight } = target
+    if (scrollHeight - scrollTop === clientHeight) {
+      console.log('finish!')
+    }
+  }
   const onHostPointClick = (id) => {
     history.push('/hostpoint/' + id)
   }
 
   return (
-    <div className={s.container}>
+    <div className={s.container} onScroll={handleScroll}>
       <header className={s.header}>Все заведения</header>
       <p className={s.description}>Все заведения в которых можно поставить штамп</p>
       {
         data.map((hostPoint, id) =>
-          <div  onClick={() => onHostPointClick(id)} key={id}>
+          <div onClick={() => onHostPointClick(id)} key={id}>
             <HostPoint hostPoint={hostPoint}/>
           </div>,
         )
@@ -22,4 +29,4 @@ const Organisations = ({ data = [], history }) => {
   )
 }
 
-export default Organisations
+export default HostPoints
