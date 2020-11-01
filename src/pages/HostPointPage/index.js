48f9be49231537ddaae994c0c2d6090ccc451c4f
zhendozzz <info@zhendozzz.ru>
2020-11-01 18:21:23 +0300

    add host point page

---
+++ src/pages/HostPointPage/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react'
+import s from './HostPointPage.module.scss'
+import { YMaps, Map, Placemark } from 'react-yandex-maps'
+
+const HostPointPage = ({ data, match }) => {
+  const { params } = match
+  const { id } = params
+  const hostPoint = data.find((item, number) => {
+    return number === Number(id)
+  })
+  const { location } = hostPoint
+  const { longitude, latitude } = location
+  const mapData = {
+    center: [latitude, longitude],
+    zoom: 18,
+  }
+  const placeMarkOptions = {
+    center: [latitude, longitude],
+    preset: 'islands#redIcon',
+    // Setting the placemark color (in RGB format).
+    iconColor: '#ff0000',
+  }
+  const placeMarkGeometry = [latitude, longitude]
+  return (
+    <YMaps>
+      <Map className={s.container} defaultState={mapData}>
+        <Placemark defaultOptions={placeMarkOptions} defaultGeometry={placeMarkGeometry}/>
+      </Map>
+    </YMaps>
+  )
+}
+export default HostPointPage
